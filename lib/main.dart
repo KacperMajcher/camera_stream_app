8e5925e66eb0a278e1d93c38d83b85fb090ccd63
Kacper <kacpermajcher13@gmail.com>
2026-03-31 12:39:24 +0200

    build: add vector_math dependency and set preferred orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:camera/camera.dart';
 import 'package:camera_stream_app/src/screens/camera_stream_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   final cameras = await availableCameras();
   CameraDescription? backCamera;
   if (cameras.isNotEmpty) {
